6279891c8c13487b7df4fe758350ca06c16dd59a
intodeep55555 <intodeep55555@gmail.com>
2023-10-07 23:14:54 +0900

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import firebase from "./FirebaseConfig"; //this is not import from package but config created by me.
+
+function App() {
+  return (
+    <div className="App">
+      <div className='title-row'>
+        <h1 className='title'>Firebase Recipes</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
